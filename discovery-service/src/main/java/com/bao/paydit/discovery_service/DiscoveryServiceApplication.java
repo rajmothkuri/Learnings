2eac298c774e3d1d9ac61902fff053419e40f35a
Rajavardhan Mothkuri <rajavardhan.mothkuri@gmail.com>
2026-05-18 17:20:29 +0000

    discovery-service setup

---
+++ discovery-service/src/main/java/com/bao/paydit/discovery_service/DiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.boa.paydit.discovery_service;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+
+}
